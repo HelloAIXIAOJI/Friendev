6e5f0e2d38d57c44034e656047bf3ab5281fd53d
HelloAIXIAOJI <aixiaoji2020@163.com>
2025-11-15 07:30:36 +0800

    Adjusted the code order at startup

--- src/main.rs
+++ src/main.rs
@@ -37,6 +37,9 @@ async fn main() -> Result<()> {
     
     println!("\x1b[32m[OK]\x1b[0m \x1b[2m{}\x1b[0m\n", i18n.get("config_loaded"));
 
+    // 自动删除0消息的历史文件
+    ChatSession::cleanup_empty_sessions()?;
+
     // 获取当前工作目录
     let working_dir = env::current_dir()?;
     println!("\x1b[36m[DIR]\x1b[0m \x1b[2m{}\x1b[0m\n", working_dir.display());
@@ -45,9 +48,6 @@ async fn main() -> Result<()> {
     let mut session = ChatSession::new(working_dir.clone());
     session.save()?;
     println!("\x1b[32m[OK]\x1b[0m \x1b[2m{}:\x1b[0m \x1b[90m{}\x1b[0m\n", i18n.get("new_session"), session.id);
-    
-    // 自动删除0消息的历史文件
-    ChatSession::cleanup_empty_sessions()?;
 
     // 创建 API 客户端
     let mut api_client = ApiClient::new(config.clone());
